e5514734a6b17cd676e10ea950eb4da52b654d10
cireel <cirelabe1@gmail.com>
2025-10-26 18:55:08 +0000

    commit du dmanche

--- teste.py
+++ teste.py
@@ -1,1 +1,7 @@
-print('Hello, world')
+numerateur = float(input('saisir ici : '))
+deno =float(input('saisi ici : '))
+if deno != 0 :
+    quotient = numerateur / deno
+    print('le quotient est egale a :',quotient)
+else:
+    print('le deno ne peut etre egale a 0')
